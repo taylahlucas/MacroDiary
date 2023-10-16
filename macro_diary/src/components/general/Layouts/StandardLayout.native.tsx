a36a9c393588725454fb3d79f6df33b0966d968a
Taylah Lucas <Taylah.lucas@hunterindustries.com>
2023-10-16 17:21:09 +0100

    added optional background

--- macro_diary/src/components/general/Layouts/StandardLayout.native.tsx
+++ macro_diary/src/components/general/Layouts/StandardLayout.native.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StandardLayoutContainer } from './StandardLayoutStyledComponents.native';
 import useGetTheme from '../../../styles/hooks/useGetTheme.native';
+import { Image } from 'react-native';
 
 interface StandardLayoutProps {
   children: any;
@@ -10,9 +11,16 @@ const StandardLayout = ({ children }: StandardLayoutProps) => {
   const theme = useGetTheme();
 
   return (
-    <StandardLayoutContainer backgroundColor={theme.black}>
+    <>
+      <Image 
+        source={require('../../../assets/images/app_background.png')}
+        style={{ flex: 1, resizeMode: 'cover', width: '100%', height: '100%', zIndex: -1 }}
+      />
+      {/* TODO: Decide on whether to use background or not backgroundColor={theme.darkGrey} */}
+      <StandardLayoutContainer backgroundColor={theme.black}>
       {children}
     </StandardLayoutContainer>
+    </>
   );
 };
 
--- macro_diary/src/components/general/Layouts/StandardLayoutStyledComponents.native.tsx
+++ macro_diary/src/components/general/Layouts/StandardLayoutStyledComponents.native.tsx
@@ -4,9 +4,12 @@ interface StandardLayoutContainerProps {
   backgroundColor: string;
 }
 
-export const StandardLayoutContainer = styled.SafeAreaView<StandardLayoutContainerProps>`
-  background-color: ${props => props.backgroundColor};
+//<StandardLayoutContainerProps>
+//  background-color: ${props => props.backgroundColor};
+export const StandardLayoutContainer = styled.SafeAreaView`
   width: 100%;
   height: 100%;
   align-items: center;
+  position: absolute;
+  flex: 1;
 `;
